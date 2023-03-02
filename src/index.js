5c27de105496035f8129818412b94f5962c70c1c
tthompson899 <tdthompson899@gmail.com>
2023-03-01 20:05:01 -0600

    redux state project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + action.payload;
+    case "DECREMENT":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log("current state", store.getState());
+});
+
+store.dispatch({
+  type: "INCREMENT",
+  payload: 1
+});
+
+store.dispatch({
+  type: "INCREMENT",
+  payload: 5
+});
+
+store.dispatch({
+  type: "DECREMENT",
+  payload: 2
+});
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <h3>Open console to see the output.</h3>
+  </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
